30099ced3fc624fc949b868cfc58c88867744760
jshandil <shandilyajitmanyu6@gmail.com>
2024-09-02 11:26:26 -0700

    newtest

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,9 @@ test('renders learn react link test 2', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link test 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
